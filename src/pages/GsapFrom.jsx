7f765af911600fcb2093177bb238db79745d7075
Sheikh Mohammad Rakib <u2009023@student.cuet.ac.bd>
2026-01-01 11:12:29 +0600

    Adds GSAP animation to green box on mount
    Integrates GSAP to animate a green box with continuous rotation
    and horizontal movement, enhancing visual engagement and
    demonstrating usage of animation hooks for interactive UI.
    
    Adds continuous GSAP animation to green box on mount
    
    Integrates GSAP animation to rotate and move the green box horizontally in a looping, interactive manner. Enhances visual engagement and demonstrates effective use of animation hooks for dynamic UI behavior.

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(()=> {
+    gsap.from('#green-box', {
+      rotation: 360,
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      duration: 5,
+      ease: 'power1.inOut',
+    })
+  }, []);
 
   return (
     <main>
